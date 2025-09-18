ef6dac2a10a30472129a6a823077cebb80ab6874
Vinicius Duarte Rosa <viniciusduarterosa@icloud.com>
2025-09-18 16:11:01 -0300

    feat: Configura build e roteamento para subdiretório

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+
+  base: '/IQBR/', // <-- ADICIONE ESTA LINHA AQUI
+
   server: {
     host: "::",
     port: 8080,
